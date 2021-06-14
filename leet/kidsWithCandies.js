7158af9284ddf30a2cdc0803c5f2aec87329be5d
Soo-Beom <soobeomkim77@gmail.com>
2021-06-14 16:21:59 -0700

    faster solution to kidsWithCandies

--- leet/kidsWithCandies.js
+++ leet/kidsWithCandies.js
@@ -1,3 +1,18 @@
+function fasterKidsWithCandies(candies, extraCandies) {
+    let result = []
+    let max = Math.max(...candies)
+    for (let i = 0; i < candies.length; i++){
+        if ((candies[i] + extraCandies) >= max){
+            result.push(true)
+        } else {
+            result.push(false)
+        }
+    }
+    return result
+    //80ms 72% of JS submissions
+};
+
+
 function kidsWithCandies(candies, extraCandies) {
     let result = []
     let x = [...candies]
@@ -11,4 +26,9 @@ function kidsWithCandies(candies, extraCandies) {
         }
     }
     return result
+    //117ms, 7% faster
 };
+
+// (Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+
+//     For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies.)
